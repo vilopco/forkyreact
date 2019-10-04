ff2fa8ddaccc09674b0ce136b90c9d688d6d6e9a
Christian <christianleyva@vilop.com>
2019-10-03 21:11:01 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -9,20 +9,20 @@ class App extends Component {
       textareacontent: "",
       caracteres: 0
     };
-    this.contador = this.contador.bind(this)
+    this.contador=this.contador.bind(this)
   }
 
   render() {
     return (
       <div className="container">
-        <textarea rows="3" value={this.state.textareacontent} onKeyUp={this.contador} onChange={this.contador}></textarea>
+        <textarea rows="3" value={this.state.textareacontent} onChange={this.contador}></textarea>
         <div className="counter">{this.state.caracteres}</div>
       </div>
     );
   }
   
   contador(event) {
-    let valor=this.state.textareacontent.length;
+    let valor=event.target.value.length;
     this.setState({
       caracteres: valor,
       textareacontent:event.target.value
