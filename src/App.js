d3f9e6d3c2a8e10110d7cc5491cff7ed044ea5a0
Christian <christianleyva@vilop.com>
2019-10-07 20:22:38 -0400

    Chulito

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
-import Welcome from './Welcome';
 
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
-      names: ["Juan", "Pedro", "Germán"]
+      contenido:""
     }
+    this.toggle = this.toggle.bind(this);
   }
-
   render() {
     return (
-      <div>
-        {this.state.names.map((name) =>
-          <Welcome name={name} key={name} />
-        )}
+      <div className="wrapper">
+        <label><input type="checkbox" onChange={this.toggle}/> Mostrar información importante</label>
+        <p>{this.state.contenido}</p>
       </div>
     );
+  }
+  toggle(event) {
+    if(event.target.checked){
+      this.setState({
+        contenido:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      })
+    }else{
+      this.setState({
+        contenido:""
+      });
+    }    
   }
 }
 
