21303f91c6a87451901314b78ca6a202ac7adeb0
Christian <christianleyva@vilop.com>
2019-10-07 20:33:13 -0400

    Chulito

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      contenido:""
+      contenido:"",
+      chulo:false
     }
     this.toggle = this.toggle.bind(this);
   }
@@ -13,20 +14,21 @@ class App extends Component {
     return (
       <div className="wrapper">
         <label><input type="checkbox" onChange={this.toggle}/> Mostrar información importante</label>
-        <p>{this.state.contenido}</p>
+        {this.state.chulo?<p>{this.state.contenido}</p>:''}
       </div>
     );
   }
   toggle(event) {
+    this.setState({
+      contenido:"",
+      chulo:false
+    });
     if(event.target.checked){
       this.setState({
-        contenido:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        contenido:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        chulo:true
       })
-    }else{
-      this.setState({
-        contenido:""
-      });
-    }    
+    }   
   }
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -3,23 +3,25 @@ import { shallow } from 'enzyme';
 import App from './App';
 import Setup from './Setup';
 
-it('renders without crashing', () => {
+it('renders App without crashing', () => {
   shallow(<App />);
 });
 
-it('renders the textarea', () => {
+it('renders the checkbox', () => {
   const wrapper = shallow(<App />);
-  expect(wrapper.find('textarea').length).toBe(1);
+  const checkbox = wrapper.find('input[type="checkbox"]');
+  expect(checkbox.length).toBe(1);
+  expect(checkbox.props().checked).toBeFalsy();
 });
 
-it('renders the counter', () => {
+it("doesn't renders the paragraph", () => {
   const wrapper = shallow(<App />);
-  expect(wrapper.find('.counter').length).toBe(1);
-  expect(wrapper.find('.counter').text()).toBe("0");
+  expect(wrapper.find('p').length).toBe(0);
 });
 
-it('updates the counter', () => {
+it("shows the paragraph when checkbox is checked", () => {
   const wrapper = shallow(<App />);
-  wrapper.find('textarea').simulate('change', { target: { value: 'Hola Mundo' } });
-  expect(wrapper.find('.counter').text()).toBe("0");
+  wrapper.find('input[type="checkbox"]').simulate('click');
+  wrapper.find('input[type="checkbox"]').simulate('change', { target: { checked: true } });
+  expect(wrapper.find('p').length).toBe(1);
 });
