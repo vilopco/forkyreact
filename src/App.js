b17d45c0c7d82bf00df59a4bba99755d397407dc
Christian <christianleyva@vilop.com>
2019-10-07 20:41:04 -0400

    Chulito

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      contenido:"",
       chulo:false
     }
     this.toggle = this.toggle.bind(this);
@@ -14,21 +13,14 @@ class App extends Component {
     return (
       <div className="wrapper">
         <label><input type="checkbox" onChange={this.toggle}/> Mostrar información importante</label>
-        {this.state.chulo?<p>{this.state.contenido}</p>:''}
+        {this.state.chulo?<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>:''}
       </div>
     );
   }
   toggle(event) {
     this.setState({
-      contenido:"",
-      chulo:false
+      chulo:event.target.checked
     });
-    if(event.target.checked){
-      this.setState({
-        contenido:"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        chulo:true
-      })
-    }   
   }
 }
 
